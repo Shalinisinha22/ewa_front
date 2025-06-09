992ac51cd0e47af0235fedbcc2c34ed5b530f867
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2025-06-09 23:43:28 +0530

    vercel commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+   build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+  },
 })
