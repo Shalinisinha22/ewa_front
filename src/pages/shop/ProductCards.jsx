91a2b654f6412a73a5ffbbbc8ce5a47924fd2e05
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2025-06-10 23:15:09 +0530

    vercel deploy

--- src/pages/shop/ProductCards.jsx
+++ src/pages/shop/ProductCards.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import Ratingstars from '../../Components/ratingstars';
+import Ratingstars from '../../Components/Ratingstars'
 import { addToCart } from '../../redux/cartSlice';
 
 const ProductCards = ({products}) => {
