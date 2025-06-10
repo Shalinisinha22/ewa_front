90b4b79eb982ce6133b8497dca807110e5f7b405
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2025-06-10 23:02:06 +0530

    vercel deploy2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Navbar from './components/Navbar';
-import Footer from "./components/Footer"
+import Footer from "./components/"
 import './App.css'; 
 
 function App() {
