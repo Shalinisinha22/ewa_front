8cfcedf07c37f4d2a0ff8c243dbe9393e325b050
Shalini Sinha <122859073+Shalinisinha22@users.noreply.github.com>
2025-07-19 13:42:08 +0530

    Fix BlogSection import error

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import Categories from './Categories';
 import HeroSection from './HeroSection';
 import TrendingProducts from '../shop/TrendingProducts';
 import PromoBanner from './PromoBanner';
-import BlogSection from './BlogSection';
+import BlogSection from './BlogSection.jsx';
 
 const Home = () => {
   return (
